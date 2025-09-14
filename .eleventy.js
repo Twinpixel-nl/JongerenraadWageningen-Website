62453db6e2eee0fb42f5d9938c7586256275a679
Twan Meurs <tmeurs@student.che.nl>
2025-09-14 20:22:42 +0200

    Refactor Eleventy configuration, update package.json, and implement new layout and styles for Jongerenraad Wageningen website

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 module.exports = function(eleventyConfig) {
-  
-  // Kopieer alleen de assets map vanuit src.
+  // We voegen alleen de assets toe, verder geen configuratie die kan storen.
   eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
+    // Deze regels zijn cruciaal om .html als template te zien.
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
+
     dir: {
       input: "src",
       output: "_site",
